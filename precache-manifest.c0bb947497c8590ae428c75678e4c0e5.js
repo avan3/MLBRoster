1e69c5a66d4e8473956a11b84b1297a9d5ded84e
avan3 <andrew.van1@live.com>
2019-09-02 16:54:31 -0400

    Updates

---
+++ precache-manifest.c0bb947497c8590ae428c75678e4c0e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6b29dbbd5584ae69ed4875117b26bb",
+    "url": "/MLBRoster/index.html"
+  },
+  {
+    "revision": "cc8e2033158cf994aac8",
+    "url": "/MLBRoster/static/css/main.5edef878.chunk.css"
+  },
+  {
+    "revision": "00ad3ac20e1c2f83a67d",
+    "url": "/MLBRoster/static/js/2.b67b75c7.chunk.js"
+  },
+  {
+    "revision": "cc8e2033158cf994aac8",
+    "url": "/MLBRoster/static/js/main.7f27f6c5.chunk.js"
+  },
+  {
+    "revision": "d06b519f6bc9b95c64bc",
+    "url": "/MLBRoster/static/js/runtime~main.3815efa1.js"
+  }
+]);
